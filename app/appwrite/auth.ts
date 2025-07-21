3579163561cb4f3d830e8f0c6f4967ca0b842827
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-07-21 12:34:17 +0530

    auth setup

--- app/appwrite/auth.ts
+++ app/appwrite/auth.ts
@@ -1,4 +1,4 @@
-import { OAuthProvider, Query } from "appwrite";
+import { ID, OAuthProvider, Query } from "appwrite";
 import { account, appwriteConfig, database } from "./client";
 import { redirect } from "react-router";
 
@@ -12,8 +12,10 @@ export const loginWithGoogle = async () => {
 
 export const logoutUser = async () => {
   try {
+    await account.deleteSession("current");
+    return true;
   } catch (error) {
-    console.log(error);
+    console.log("logout", error);
   }
 };
 
@@ -26,6 +28,74 @@ export const getUser = async () => {
 
 export const getGooglePicture = async () => {
   try {
+    const session = await account.getSession("current");
+    const authToken = session.providerAccessToken;
+
+    if (!authToken) {
+      console.log("auth token not available");
+      return null;
+    }
+
+    const response = await fetch(
+      "https://people.googleapis.com/v1/people/me?personFields=photos",
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.log("failed to fetch proflie photo");
+      return null;
+    }
+
+    const data = await response.json();
+
+    const photoUrl =
+      data.photos && data.photos.length > 0 ? data.photos[0].url : null;
+    return photoUrl;
+  } catch (error) {
+    console.log("getphoto", error);
+  }
+};
+
+export const storeUserData = async () => {
+  try {
+    const user = await account.get();
+
+    if (!user) return null;
+
+    const { documents } = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", user.$id)]
+    );
+
+    if (documents.length > 0) {
+      return documents[0];
+    }
+
+    const newUser = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        accountId: user.$id,
+        email: user.email,
+        name: user.name,
+        imageUrl: imageUrl || "",
+        joinedAt: new Date().toISOString(),
+      }
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log("store", error);
+  }
+};
+export const getExistingUser = async () => {
+  try {
     const user = await account.get();
     if (!user) return redirect("/sign-in");
 
@@ -37,19 +107,6 @@ export const getGooglePicture = async () => {
         Query.select(["name", "email", "imageUrl", "joinedAt", "accountId"]),
       ]
     );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const storeUserData = async () => {
-  try {
-  } catch (error) {
-    console.log(error);
-  }
-};
-export const getExistingUser = async () => {
-  try {
   } catch (error) {
     console.log(error);
   }
